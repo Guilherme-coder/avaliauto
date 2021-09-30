f87c04beb80720f2674f1487b95af471d19b6ee9
Guilherme Hendres <ghendressmelo@gmail.com>
2021-09-30 03:02:43 +0000

    inicio das implementações de sessões de usuário

--- models.py
+++ models.py
@@ -1,12 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-class Consultoria(models.Model):
-    data = models.DateTimeField('data')
-    local = models.CharField(max_length = 300)
+class Veiculo(models.Model):
     detalhamento = models.CharField(max_length = 300)
     fabricanteVeiculo = models.CharField(max_length = 100)
     modeloVeiculo = models.CharField(max_length = 100)
     anoVeiculo = models.CharField(max_length = 4)
+
+class Cliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome = models.CharField(max_length = 150)
+    telefone = models.CharField(max_length = 15)
+
+class Mecanico(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome = models.CharField(max_length = 150)
+    telefone = models.CharField(max_length = 15)
+
+class Consultoria(models.Model):
+    data = models.DateTimeField('data')
+    local = models.CharField(max_length = 300)
     temConsultor = models.CharField(max_length = 1)
     plano = models.CharField(max_length = 20)
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE, default=1)
+    cliente = models.IntegerField()
+    mecanico = models.IntegerField()
--- views.py
+++ views.py
@@ -1,10 +1,77 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .models import Cliente, Mecanico, Veiculo
 from django.urls import reverse
 
 from .models import Consultoria
 
 # Create your views here.
+def auth_in(request):
+    username = request.POST['user']
+    password = request.POST['password']
+    user = authenticate(
+        request,
+        username,
+        password
+    )
+    if user is not None:
+        login(request)
+        return user(request)
+    return render(request, 'avaliauto/login.html')
+
+def auth_out(request):
+    logout(request)
+
+def auth_up_client(request):
+    nome = request.POST['name']
+    username = request.POST['username']
+    email = request.POST['email']
+    senha = request.POST['senha']
+    nome = request.POST['name']
+    user = User.objects.create_user(
+        first_name = nome,
+        email = email,
+        username = username,
+        password = senha
+    )
+    user.save()
+
+    nome_cliente = request.POST['nome_cliente']
+    telefone = request.POST['telefone']
+    cliente = Cliente.objects.create(
+        user = user.id,
+        nome = nome_cliente,
+        telefone = telefone
+    )
+    cliente.save()
+    return render(request, 'avaliauto/home.html')
+
+
+def auth_up_mecanico(request):
+    nome = request.POST['nome']
+    email = request.POST['email']
+    username = request.POST['username']
+    senha = request.POST['senha']
+    user = User.objects.create_user(
+        first_name = nome,
+        email = email,
+        username = username,
+        password = senha
+    )
+    user.save()
+
+    nome_mecanico = request.POST['nome_mecanico']
+    telefone = request.POST['telefone']
+    mecanico = Mecanico.objects.create(
+        user = user.id,
+        nome = nome_mecanico,
+        telefone = telefone
+    )
+    mecanico.save()
+    return render(request, 'avaliauto/home.html')
+
 
 def home(request):
     return render(request, 'avaliauto/home.html')
@@ -19,7 +86,7 @@ def index(request):
 
 
 def show(request):
-    consultorias = Consultoria.objects.filter(temConsultor='N')
+    consultorias = Consultoria.objects.filter(mecanico=1)
 
     context = {
         'consultorias': consultorias
@@ -36,9 +103,24 @@ def inserir(request):
     plan = 'Plus'
     model = request.POST['modeloVeiculo']
     ano = request.POST['anoVeiculo']
-    consul = 'N'
 
-    consultoria = Consultoria(data = dt, local = loc, detalhamento = det, fabricanteVeiculo = fab, plano = plan, modeloVeiculo = model, anoVeiculo = ano, temConsultor = consul)
+    veiculo = Veiculo(
+        detalhamento = det,
+        fabricanteVeiculo = fab,
+        modeloVeiculo = model,
+        anoVeiculo = ano
+    )
+    veiculo.save()
+
+    consultoria = Consultoria(
+        data = dt,
+        local = loc,
+        plano = plan,
+        veiculo = veiculo,
+        #cliente = request.user.id,
+        cliente = 1,
+        mecanico = 1
+    )
     consultoria.save()
 
     return HttpResponseRedirect("/avaliauto/")
@@ -49,20 +131,25 @@ def aceitarConsultoria(request, id):
 
     dt = consultoria.data
     loc = consultoria.local
-    det = consultoria.detalhamento
-    fab = consultoria.fabricanteVeiculo
     plan = consultoria.plano
-    model = consultoria.modeloVeiculo
-    ano = consultoria.anoVeiculo
 
-    consultoria = Consultoria(id = consultoria.id, data = dt, local = loc, detalhamento = det, fabricanteVeiculo = fab, plano = plan, modeloVeiculo = model, anoVeiculo = ano, temConsultor = "Y")
+    consultoria = Consultoria(
+        id = consultoria.id,
+        data = dt,
+        local = loc,
+        plano = plan,
+        veiculo = consultoria.veiculo,
+        cliente = consultoria.cliente,
+        #mecanico = request.user.id,
+        mecanico = 2
+    )
 
     consultoria.save()
     return HttpResponseRedirect("/avaliauto/minhasConsultorias")
 
 
 def minhas_consultorias(request):
-    consultorias = Consultoria.objects.filter(temConsultor='Y')
+    consultorias = Consultoria.objects.exclude(mecanico = 1)
 
     context = {
         'consultorias': consultorias
